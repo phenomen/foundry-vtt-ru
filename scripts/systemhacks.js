c66a25c498f60bd16b54097cdb55bce6915d1449
Aleksandr Vasilenko <phenomen@pm.me>
2021-02-14 21:41:27 -0600

    Update systemhacks.js

--- scripts/systemhacks.js
+++ scripts/systemhacks.js
@@ -2,7 +2,6 @@ Hooks.once("init", async () => {
   loadCSS("modules/ru-RU/styles/" + game.system.id + ".css");
 
   if (game.system.id === "dnd5e") {
-    // Sort skills alphabetically
     async function sortSkillsAlpha() {
       const lists = document.getElementsByClassName("skills-list");
       for (let list of lists) {
@@ -14,7 +13,32 @@ Hooks.once("init", async () => {
           }
         }
         complist.sort(function (a, b) {
-          return a.innerText > b.innerText ? 1 : -1;
+          return a.innerText.localeCompare(b.innerText);
+        });
+        for (let sk of complist) {
+          list.appendChild(sk);
+        }
+      }
+    }
+
+    Hooks.on("renderActorSheet", async function () {
+      sortSkillsAlpha();
+    });
+  }
+
+  if (game.system.id === "deltagreen") {
+    async function sortSkillsAlpha() {
+      const lists = document.getElementsByClassName("grid grid-3col");
+      for (let list of lists) {
+        const competences = list.childNodes;
+        let complist = [];
+        for (let sk of competences) {
+          if (sk.innerText && sk.tagName == "DIV") {
+            complist.push(sk);
+          }
+        }
+        complist.sort(function (a, b) {
+          return a.innerText.localeCompare(b.innerText);
         });
         for (let sk of complist) {
           list.appendChild(sk);
