7b0ae69c12a2ba90a6a0b475ffb650c9c3f1feb6
Aleksandr Vasilenko <phenomen@pm.me>
2021-04-18 01:59:54 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,7 @@
 const gulp = require('gulp');
 const merge = require('gulp-merge-json');
-const flatten = require('gulp-flat');
 
 function defaultTask(cb) {
-
-    gulp.src('./i18n/en_US/*.json')
-        .pipe(flatten())
-        .pipe(gulp.dest('./i18n/en_US'))
-
-    gulp.src('./i18n/ru_RU/*.json')
-        .pipe(flatten())
-        .pipe(gulp.dest('./i18n/ru_RU'))
 
     gulp.src('./i18n/ru_RU/*.json')
         .pipe(merge())
