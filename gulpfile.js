9a8f9b94461c6d60d98b4dc0c4cb4c2585d05a1e
Aleksandr Vasilenko <phenomen@pm.me>
2021-04-18 01:06:54 -0500

    prepare for transifex workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
-var gulp = require('gulp');
-var merge = require('gulp-merge-json');
+const gulp = require('gulp');
+const merge = require('gulp-merge-json');
+const flatten = require('gulp-flat');
 
 function defaultTask(cb) {
 
-    gulp.src('./source/*.json')
+    gulp.src('./i18n/en_US/*.json')
+        .pipe(flatten())
+        .pipe(gulp.dest('./i18n/en_US'))
+
+    gulp.src('./i18n/ru_RU/*.json')
+        .pipe(flatten())
+        .pipe(gulp.dest('./i18n/ru_RU'))
+
+    gulp.src('./i18n/ru_RU/*.json')
         .pipe(merge())
         .pipe(gulp.dest('./dist'));
 
