9ac8e6c462309896afae2332b753f4778ea1d6f7
Aleks <phenomen@pm.me>
2021-06-23 10:03:01 -0500

    Smart Target and Tidy UI

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 const gulp = require('gulp');
 const merge = require('gulp-merge-json');
+const flatten = require('gulp-flat');
 
-function defaultTask(cb) {
+gulp.task('merge',
+    () => gulp.src('./i18n/modules/*.json')
+		 .pipe(merge())
+         .pipe(gulp.dest('dist'))
+);
 
-    gulp.src('./i18n/modules/*.json')
-        .pipe(merge())
-        .pipe(gulp.dest('./dist'));
+gulp.task('flatten',
+    () => gulp.src('./temp/unflattened/*.json')
+		 .pipe(flatten())
+         .pipe(gulp.dest('./temp/flattened/'))
+);
 
-    cb();
-}
-
-exports.default = defaultTask
