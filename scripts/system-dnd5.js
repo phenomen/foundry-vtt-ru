006cc95823c20db04e6bc87a3563bd25945b6e82
Aleks <phenomen@pm.me>
2021-10-22 14:02:45 -0500

    D&D5 Spells & SFRPG Fix

--- scripts/system-dnd5.js
+++ scripts/system-dnd5.js
@@ -1,24 +1,42 @@
 export function InitDND5() {
-    async function sortSkillsAlpha() {
-        const lists = document.getElementsByClassName("skills-list");
-        for (let list of lists) {
-            const competences = list.childNodes;
-            let complist = [];
-            for (let sk of competences) {
-                if (sk.innerText && sk.tagName == "LI") {
-                    complist.push(sk);
-                }
-            }
-            complist.sort(function(a, b) {
-                return a.innerText.localeCompare(b.innerText);
-            });
-            for (let sk of complist) {
-                list.appendChild(sk);
-            }
+  async function sortSkillsAlpha() {
+    const lists = document.getElementsByClassName("skills-list");
+    for (let list of lists) {
+      const competences = list.childNodes;
+      let complist = [];
+      for (let sk of competences) {
+        if (sk.innerText && sk.tagName == "LI") {
+          complist.push(sk);
         }
+      }
+      complist.sort(function (a, b) {
+        return a.innerText.localeCompare(b.innerText);
+      });
+      for (let sk of complist) {
+        list.appendChild(sk);
+      }
     }
+  }
 
-    Hooks.on("renderActorSheet", async function() {
-        sortSkillsAlpha();
+  Hooks.on("renderActorSheet", async function () {
+    sortSkillsAlpha();
+  });
+
+  if (typeof Babele === "undefined") {
+    new Dialog({
+      title: "Перевод библиотек",
+      content: `<p>Для перевода библиотек системы D&D5 требуется установить и активировать модуль Babele.<p>`,
+      buttons: {
+        done: {
+          label: "Хорошо",
+        },
+      },
+    }).render(true);
+  } else {
+    Babele.get().register({
+      module: "ru-ru",
+      lang: "ru",
+      dir: "compendium/dnd5e",
     });
+  }
 }
