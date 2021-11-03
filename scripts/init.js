400aacdc655e06a079fdd066bd6d2b6be0242ae3
Aleks <phenomen@pm.me>
2021-11-03 17:05:41 -0500

    0.9.3.46 - Ctrl+C/V/Z теперь работает с раскладкой кириллицы.
    Для работы требуется модуль libWrapper.

--- scripts/init.js
+++ scripts/init.js
@@ -39,3 +39,43 @@ Hooks.once("init", async () => {
     InitALIEN();
   }
 });
+
+Hooks.once("ready", async () => {
+  if (typeof libWrapper === "function") {
+    libWrapper.register(
+      "ru-ru",
+      "game.keyboard._handleKeys",
+      handleKeysCyrillic,
+      "OVERRIDE"
+    );
+
+    function handleKeysCyrillic(event, key, up) {
+      const modifiers = {
+        key: key,
+        isShift: event.shiftKey,
+        isCtrl: event.ctrlKey || event.metaKey,
+        isAlt: event.altKey,
+        hasFocus: this.hasFocus,
+        hasModifier:
+          event.shiftKey || event.ctrlKey || event.metaKey || event.altKey,
+      };
+
+      if (key === "Tab") this._onTab(event, up, modifiers);
+      else if (key === "Escape") this._onEscape(event, up, modifiers);
+      else if (key === "Space") this._onSpace(event, up, modifiers);
+      else if (key in this.moveKeys) this._onMovement(event, up, modifiers);
+      else if (this.digitKeys.includes(key))
+        this._onDigit(event, up, modifiers);
+      else if (["Delete", "Backspace"].includes(key))
+        this._onDelete(event, up, modifiers);
+      else if (key === "Alt") this._onAlt(event, up, modifiers);
+      else if (key.toLowerCase() === "z" || key.toLowerCase() === "я")
+        this._onKeyZ(event, up, modifiers);
+      else if (key.toLowerCase() === "c" || key.toLowerCase() === "с")
+        this._onKeyC(event, up, modifiers);
+      else if (key.toLowerCase() === "v" || key.toLowerCase() === "м")
+        this._onKeyV(event, up, modifiers);
+      else if (key in this.zoomKeys) this._onKeyZoom(event, up, modifiers);
+    }
+  }
+});
