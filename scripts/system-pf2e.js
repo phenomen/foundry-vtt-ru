ee957a1044c98a5d2d21102711d43f9515e7ab33
Aleksandr Vasilenko <phenomen@pm.me>
2022-01-04 00:45:50 -0600

    PF2: Fixed Translated Conditions

--- scripts/system-pf2e.js
+++ scripts/system-pf2e.js
@@ -6,70 +6,10 @@ export function InitPF2E() {
       dir: "compendium/pf2e",
     });
 
-    const translatedConditions = new Collection([
-      ["Blinded", "Слепота"],
-      ["Broken", "Поломка"],
-      ["Clumsy", "Неуклюжесть"],
-      ["Concealed", "Плохая видимость"],
-      ["Confused", "Замешательство"],
-      ["Controlled", "Контроль"],
-      ["Dazzled", "Растерянность"],
-      ["Deafened", "Глухота"],
-      ["Doomed", "Обречённость"],
-      ["Drained", "Истощение"],
-      ["Dying", "При смерти"],
-      ["Encumbered", "Нагруженность"],
-      ["Enfeebled", "Слабость"],
-      ["Fascinated", "Завороженность"],
-      ["Fatigued", "Утомление"],
-      ["Flat-Footed", "Застигнутость врасплох"],
-      ["Fleeing", "Паника"],
-      ["Friendly", "Дружелюбие"],
-      ["Frightened", "Испуг"],
-      ["Grabbed", "Захват"],
-      ["Helpful", "Готовность помочь"],
-      ["Hidden", "Спрятанность"],
-      ["Hostile", "Враждебность"],
-      ["Immobilized", "Неподвижность"],
-      ["Immobilized", "Неподвижность"],
-      ["Indifferent", "Безразличие"],
-      ["Invisible", "Невидимость"],
-      ["Observed", "Видимость"],
-      ["Paralyzed", "Паралич"],
-      ["Persistent Damage", "Продолжительный урон"],
-      ["Petrified", "Окаменение"],
-      ["Prone", "Распластанность"],
-      ["Quickened", "Ускорение"],
-      ["Restrained", "Обездвиженность"],
-      ["Sickened", "Тошнота"],
-      ["Slowed", "Замедление"],
-      ["Stunned", "Шок"],
-      ["Stupefied", "Ошеломление"],
-      ["Unconscious", "Без сознания"],
-      ["Undetected", "Необнаруженность"],
-      ["Unfriendly", "Недружелюбие"],
-      ["Unnoticed", "Незамеченность"],
-      ["Wounded", "Ранение"],
-    ]);
-
-    const translate = function (originalCondition) {
-      let translatedCondition = translatedConditions.get(originalCondition);
-      return translatedCondition ? translatedCondition : originalCondition;
-    };
-
-    Babele.get().registerConverters({
-      conditionArray: (values) => {
-        return values
-          ? values.map((value) => {
-              return { condition: translate(value.condition) };
-            })
-          : null;
-      },
-    });
-
     Hooks.once("babele.ready", () => {
       game.pf2e.ConditionManager.init();
     });
+
   } else {
     new Dialog({
       title: "Перевод библиотек",
