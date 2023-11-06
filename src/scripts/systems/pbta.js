967e8eb06f4a3ccef57a09a4c68eae2fb20ae065
Aleksandr Vasilenko <phenomen@pm.me>
2023-11-06 11:06:12 -0600

    Translation update: Foundry V11.314, systems and modules

--- src/scripts/systems/pbta.js
+++ src/scripts/systems/pbta.js
@@ -6,6 +6,8 @@ export function init() {
 				lang: "ru",
 				dir: "compendium/masks"
 			});
+
+			overrideSheet();
 		} else {
 			new Dialog({
 				title: "Перевод библиотек",
@@ -18,4 +20,275 @@ export function init() {
 			}).render(true);
 		}
 	}
+}
+
+function overrideSheet() {
+	Hooks.once("pbtaSheetConfig", () => {
+		game.settings.set("pbta", "sheetConfigOverride", true);
+		game.pbta.sheetConfig = {
+			rollFormula: "2d6",
+			statToggle: {
+				label: "Locked",
+				modifier: 0
+			},
+			rollResults: {
+				failure: {
+					start: null,
+					end: 6,
+					label: "Осложнения..."
+				},
+				partial: {
+					start: 7,
+					end: 9,
+					label: "Частичный успех"
+				},
+				success: {
+					start: 10,
+					end: null,
+					label: "Успех!"
+				}
+			},
+			actorTypes: {
+				character: {
+					stats: {
+						danger: {
+							label: "Дерзкий",
+							value: 0
+						},
+						freak: {
+							label: "Странный",
+							value: 0
+						},
+						savior: {
+							label: "Верный",
+							value: 0
+						},
+						superior: {
+							label: "Хитрый",
+							value: 0
+						},
+						mundane: {
+							label: "Обычный",
+							value: 0
+						}
+					},
+					attrTop: {
+						heroName: {
+							label: "Имя героя",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "Text",
+							value: ""
+						},
+						advances: {
+							label: "Продвижения",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "Number",
+							value: 0
+						},
+						xp: {
+							label: "Потенциал",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "Xp",
+							value: 0,
+							max: 5,
+							steps: [false, false, false, false, false]
+						},
+						momentUnlocked: {
+							label: "Момент истины",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "Checkbox",
+							checkboxLabel: "Unlocked",
+							value: false
+						}
+					},
+					attrLeft: {
+						conditions: {
+							label: "Состояния",
+							description: "Выберите все применимые:",
+							customLabel: false,
+							userLabel: false,
+							type: "ListMany",
+							condition: true,
+							options: {
+								0: {
+									label: "Напуган (-2 сталкиваясь с угрозой)",
+									value: false
+								},
+								1: {
+									label: "Зол (-2 утешая или заглядывая)",
+									value: false
+								},
+								2: {
+									label: "Виноват (-2 провоцируя или оценивая)",
+									value: false
+								},
+								3: {
+									label: "Отчаян (-2 давая волю)",
+									value: false
+								},
+								4: {
+									label: "Неуверен (-2 защищая или отвергая)",
+									value: false
+								}
+							}
+						},
+						look: {
+							label: "Внешность",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "LongText",
+							value: ""
+						},
+						abilities: {
+							label: "Способности",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "LongText",
+							value: ""
+						},
+						influence: {
+							label: "Влияние",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "LongText",
+							value: ""
+						},
+						moment: {
+							label: "Момент истины",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "LongText",
+							value: ""
+						},
+						advancements: {
+							label: "Продвижения",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "ListMany",
+							condition: false,
+							options: {
+								0: {
+									label: "[Text]",
+									value: false
+								},
+								1: {
+									label: "[Text]",
+									value: false
+								}
+							}
+						}
+					},
+					moveTypes: {
+						basic: {
+							label: "Основные ходы",
+							moves: []
+						},
+						playbook: {
+							label: "Ходы архетипа",
+							moves: []
+						},
+						team: {
+							label: "Ходы команды",
+							moves: []
+						},
+						adult: {
+							label: "Ходы взрослого",
+							moves: []
+						}
+					}
+				},
+				npc: {
+					attrTop: {
+						realName: {
+							label: "Настоящее имя",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "Text",
+							value: ""
+						}
+					},
+					attrLeft: {
+						conditions: {
+							label: "Состояния",
+							description: "Выберите все применимые:",
+							customLabel: false,
+							userLabel: false,
+							type: "ListMany",
+							condition: false,
+							options: {
+								0: {
+									label: "Напуган",
+									value: false
+								},
+								1: {
+									label: "Зол",
+									value: false
+								},
+								2: {
+									label: "Виноват",
+									value: false
+								},
+								3: {
+									label: "Отчаян",
+									value: false
+								},
+								4: {
+									label: "Неуверен",
+									value: false
+								}
+							}
+						},
+						drive: {
+							label: "Мотивация",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "LongText",
+							value: ""
+						},
+						abilities: {
+							label: "Способности",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "LongText",
+							value: ""
+						},
+						generation: {
+							label: "Поколение",
+							description: null,
+							customLabel: false,
+							userLabel: false,
+							type: "Text",
+							value: ""
+						}
+					},
+					moveTypes: {
+						villain: {
+							label: "Ходы злодея",
+							moves: []
+						},
+						condition: {
+							label: "Ходы состояния",
+							moves: []
+						}
+					}
+				}
+			}
+		};
+	});
 }
