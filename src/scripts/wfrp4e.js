5689895332fb2b148c6fd631bf6f81cae39e44b1
phenomen <phenomen@pm.me>
2024-05-05 23:39:02 -0500

    Merge scripts

--- src/scripts/wfrp4e.js
+++ src/scripts/wfrp4e.js
@@ -135,6 +135,8 @@ export function init() {
 			},
 		});
 
+		translateScripts();
+
 		Hooks.once("ready", () => {
 			patchConfigReady();
 		});
@@ -403,11 +405,19 @@ function translateCareer(item) {
 		}
 
 		if (skills) {
-			item.system.skills = translateCareerSkills(skills);
+			item.system.skills = translateCareerItems(
+				skills,
+				"skill",
+				translatedSkillSpec,
+			);
 		}
 
 		if (talents) {
-			item.system.talents = translateCareerTalents(talents);
+			item.system.talents = translateCareerItems(
+				talents,
+				"talent",
+				translatedTalentSpec,
+			);
 		}
 
 		foundry.utils.mergeObject(item, translation);
@@ -542,3 +552,50 @@ function translateEffect(item) {
 
 	return item;
 }
+
+function translateScripts() {
+	// TODO
+	/*
+ const translatedScripts = Object.fromEntries(
+ 	Object.entries(game.wfrp4e.config.effectScripts).map(
+ 		([key, value]) => [key, translateScript(value)],
+ 	),
+ );
+
+ game.wfrp4e.config.effectScripts = translatedScripts;
+ */
+}
+
+/*
+function translateScript(script) {
+	const terms = script.match(/"[^"]+"/g);
+
+	if (terms) {
+		const translatedTerms = terms.map((term) => {
+			const item = {
+				name: term,
+			};
+
+			console.log(item.name);
+
+			let translatedTerm = translateSkill(item);
+
+			if (translatedTerm.name === term) {
+				translatedTerm = translateTrait(item);
+			}
+
+			return `${translatedTerm.name}`;
+		});
+
+		let result = script;
+
+		terms.forEach((term, index) => {
+			result = result.replace(term, translatedTerms[index]);
+		});
+
+		return result;
+	}
+
+	return script;
+}
+*/
