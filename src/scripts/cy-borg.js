19f88430f58860b701e9c049b077eef1910c339c
Aleksandr Vasilenko <phenomen@pm.me>
2024-07-23 10:23:48 -0500

    cy_borg scripts and styles

--- src/scripts/cy-borg.js
+++ src/scripts/cy-borg.js
@@ -2,4 +2,22 @@ import { setupBabele } from "../shared.js";
 
 export function init() {
 	setupBabele("cy-borg");
+
+	CONFIG.CY.appBacklashesTable = "Противодействие программ";
+
+	libWrapper.register(
+		"ru-ru",
+		"CONFIG.Item.documentClass.prototype.createLinkedInfestation",
+		async function (wrapped, ...args) {
+			const infestation = await drawDocument(TABLES_PACK, "Заражения");
+			if (!infestation) {
+				console.error("Не удалось получить заражение");
+				return;
+			}
+			const data = dupeData(infestation);
+			data.system.nanoId = this.id;
+			await this.parent.createEmbeddedDocuments("Item", [data]);
+		},
+		"OVERRIDE",
+	);
 }
