8864723150e5291021712bfcabe06dcdc54c192e
Aleksandr Vasilenko <phenomen@pm.me>
2024-08-12 09:23:09 -0500

    Async fix, DSN, MATT, AoS:S

--- src/systems/dnd5e.js
+++ src/systems/dnd5e.js
@@ -113,8 +113,8 @@ export function init() {
 }
 
 /* Обновление базы AA */
-function updateAA() {
-	const translatedSettings = foundry.utils.fetchJsonWithTimeout(
+async function updateAA() {
+	const translatedSettings = await foundry.utils.fetchJsonWithTimeout(
 		"/modules/ru-ru/i18n/modules/aa-autorec.json",
 	);
 
