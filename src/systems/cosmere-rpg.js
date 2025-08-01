0730ac7c7f5481fef93171821d975c9007eddd20
phenomen <phenomen@pm.me>
2025-07-31 23:59:18 -0500

    Cosmere RPG: full system translation

--- src/systems/cosmere-rpg.js
+++ src/systems/cosmere-rpg.js
@@ -19,19 +19,19 @@ function patchConfig() {
 	CONFIG.COSMERE.ancestries.human.label = "Человек";
 	CONFIG.COSMERE.ancestries.singer.label = "Певец";
 
-	CONFIG.COSMERE.cultures.alethi.label = "Алеткар";
-	CONFIG.COSMERE.cultures.azish.label = "Азир";
-	CONFIG.COSMERE.cultures.herdazian.label = "Гердаз";
-	CONFIG.COSMERE.cultures.thaylen.label = "Тайлена";
-	CONFIG.COSMERE.cultures.unkalaki.label = "Пики рогоедов";
-	CONFIG.COSMERE.cultures.veden.label = "Йа-Кевед";
-	CONFIG.COSMERE.cultures.iriali.label = "Ири";
-	CONFIG.COSMERE.cultures.kharbranthian.label = "Харбрант";
-	CONFIG.COSMERE.cultures.listener.label = "Слушатели";
-	CONFIG.COSMERE.cultures.natan.label = "Натан";
-	CONFIG.COSMERE.cultures.reshi.label = "Решийские острова";
-	CONFIG.COSMERE.cultures.shin.label = "Шиновар";
-	CONFIG.COSMERE.cultures.wayfarer.label = "Странники"; // ???
+	CONFIG.COSMERE.cultures.alethi.label = "Алети"; // Алеткар
+	CONFIG.COSMERE.cultures.azish.label = "Азирцы"; // Азир
+	CONFIG.COSMERE.cultures.herdazian.label = "Гердазийцы"; // Гердаз
+	CONFIG.COSMERE.cultures.thaylen.label = "Тайленцы"; // Тайлена
+	CONFIG.COSMERE.cultures.unkalaki.label = "Ункалаки"; // Пики рогоедов | Альт: Рогоеды
+	CONFIG.COSMERE.cultures.veden.label = "Веденцы"; // Йа-Кевед
+	CONFIG.COSMERE.cultures.iriali.label = "Ириали"; // Ири
+	CONFIG.COSMERE.cultures.kharbranthian.label = "Харбрантийцы"; // Харбрант
+	CONFIG.COSMERE.cultures.listener.label = "Слушатели"; // Расколотые равнины | Альт: Паршенди
+	CONFIG.COSMERE.cultures.natan.label = "Натанцы"; // Натанатан
+	CONFIG.COSMERE.cultures.reshi.label = "Решийцы"; // Решийские острова
+	CONFIG.COSMERE.cultures.shin.label = "Шинцы"; // Шиновар
+	CONFIG.COSMERE.cultures.wayfarer.label = "Странники"; // Неизвестно
 }
 
 function registerConverters() {
