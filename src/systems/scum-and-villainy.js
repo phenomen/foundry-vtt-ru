b802e628395c4b87ced1a6d4c4582d0f12d5f39d
phenomen <phenomen@pm.me>
2026-05-26 22:57:54 -0500

    wip: leftover unused converters

--- src/systems/scum-and-villainy.js
+++ src/systems/scum-and-villainy.js
@@ -20,41 +20,5 @@ export function registerBabeleConverters(babele) {
         return resolveTranslatedItemName(name, { fieldTranslation, runtime });
       },
     },
-
-    translateEffects: {
-      translate(context) {
-        const { value: effects, translation: fieldTranslation, runtime } = context;
-
-        if (!effects?.length) {
-          return effects;
-        }
-
-        const effectNames = fieldTranslation?.effects;
-
-        return effects.map((effect) => {
-          const translated = foundry.utils.duplicate(effect);
-
-          if (translated.name) {
-            const localized = effectNames?.[translated.name];
-            if (localized) {
-              translated.name = localized;
-            } else {
-              translated.name = resolveTranslatedItemName(translated.name, { runtime });
-            }
-          }
-
-          if (translated.system?.changes) {
-            translated.system.changes = translated.system.changes.map((change) => {
-              if (typeof change.value === "string") {
-                change.value = resolveTranslatedItemName(change.value, { runtime });
-              }
-              return change;
-            });
-          }
-
-          return translated;
-        });
-      },
-    },
   });
 }
